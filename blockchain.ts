e68a8805450d47892e17648d4d60a72c8e73bbf2
Fynn Grandke <fynn.grandke@web.de>
2021-01-07 21:43:08 +0900

    Add block

--- blockchain.ts
+++ blockchain.ts
@@ -1,2 +1,30 @@
 import * as crypto from 'crypto'
 
+class BlockchainBlock {
+  index: number
+  timestamp: Date
+  data: Object
+  previousHash: string
+  hash = crypto.createHash('sha256')
+
+  constructor(index, timestamp, data, previousHash = '') {
+    this.index = index
+    this.timestamp = timestamp
+    this.data = data
+    this.previousHash = previousHash
+    this.setHash()
+  }
+
+  getHash() {
+    return this.hash.digest('hex')
+  }
+
+  setHash() {
+    this.hash.update(
+      this.index.toString() +
+        this.timestamp.toISOString() +
+        this.previousHash +
+        JSON.stringify(this.data)
+    )
+  }
+}
